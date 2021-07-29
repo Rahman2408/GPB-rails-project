af98d1142d4806e1c56429ef2bfeec006d2b9c4f
Rahman Elsirafy <rahman2408@gmail.com>
2021-07-29 14:43:17 -0400

    Helper methods created and added to Application Controller to establish if a user is logged and updates the navbar accordingly. Also adds some basic styling.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,20 +5,20 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(email: params[:email])
-            if user && user.authenticate(params[:password])
-                # flash[:notice] = "Sucess! Welcome back, #{user.name}!"
-                session[:user_id] = user.id
-                redirect_to root_path
-            else 
-                flash[:errors] = ["Incorrect Email or Password"]
-                redirect_to login_path
-            end
+      user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            flash[:notices] = ["Welcome back, #{user.name}!"]
+            session[:user_id] = user.id
+            redirect_to root_path
+        else 
+            flash[:errors] = ["Incorrect Email or Password"]
+            redirect_to login_path
+        end
     end
 
     def logout
         session.clear 
-        redirect_to root_path
+        redirect_to login_path
     end
-    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   get "/login", to: "sessions#login", as: "login"
   post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#logout", as: "logout"
+  match "/logout", to: "sessions#logout", via: [:get, :post]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

3e20b058dde30fe3f1427f43faf39d09ac5e1acc
Rahman Elsirafy <rahman2408@gmail.com>
2021-07-29 10:56:45 -0400

    Adds the logout method to the sessions controller with the appropriate route and adds the logout link to the view but in a partial that will serve as the app's navigation bar.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
 
     def logout
-        
+        session.clear 
+        redirect_to root_path
     end
 
-
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :projects, only: [:new, :create, :show]
   resources :users, only: [:new, :create, :show]
   
+  match '/logout', to: 'sessions#logout', via: [:get, :post]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

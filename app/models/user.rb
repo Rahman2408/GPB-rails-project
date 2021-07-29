bf795bbd07e8664f0810c9979f93eac0823b62df
Rahman Elsirafy <rahman2408@gmail.com>
2021-07-29 05:45:05 -0400

    validates all fields for siging up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_many :project_features 
     has_many :projects, through: :project_features
     has_secure_password
+    validates :email, uniqueness: true, uniqueness: {case_sensitive: false }, presence: true
+    validates :name , presence: true
+    validates :password, length: {minimum: 7}
 end

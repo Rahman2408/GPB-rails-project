49b69796268fcbf79e27da12d1afdda086dc6c8f
Rahman Elsirafy <rahman2408@gmail.com>
2021-07-29 03:43:55 -0400

    adds some data to seed file to ensure database relationships are working and creates the new.erb view for user and project models but blank without any content yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "Steve Jobs", email: "stevie@gmail.com" )
+
+Project.create(title: "Mac", goal: "Computer that puts Bill Gates out of business and is much easier to use than a Windows PC")
+
+ProjectFeature.create(name: "Messages", description: "App integrated into OS where users can read and send messages from their mac without needing to use their phone.", user_id: 1, project_id: 1 )
+

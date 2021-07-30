9e32e2a4283e5375020fcec96cae195e09b369a9
Rahman Elsirafy <rahman2408@gmail.com>
2021-07-29 21:56:12 -0400

    OAuth key and secret added as well as middleware for github log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,20 @@
 class SessionsController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: :create
 
     def login
 
     end
 
     def create
-      user = User.find_by(email: params[:email])
-        if user && user.authenticate(params[:password])
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password]) 
             flash[:notices] = ["Welcome back, #{user.name}!"]
             session[:user_id] = user.id
             redirect_to root_path
+        # elsif params[:email].blank?
+        #     @user = User.find_or_create_from_auth_hash(auth_hash)
+        #     self.current_user = @user
+        #     redirect_to root_path
         else 
             flash[:errors] = ["Incorrect Email or Password"]
             redirect_to login_path
@@ -21,4 +26,9 @@ class SessionsController < ApplicationController
         redirect_to login_path
     end
 
+    private
+
+    def auth_hash
+        # request.env.['omniauth.auth']
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#login", as: "login"
   post "/login", to: "sessions#create"
   match "/logout", to: "sessions#logout", via: [:get, :post]
-  
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post], as: "git-login" 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

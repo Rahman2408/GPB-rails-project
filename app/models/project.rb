e4a66fe7d0ba30b65a72df1e649936b1085a3cf2
Rahman Elsirafy <rahman2408@gmail.com>
2021-08-06 09:20:07 -0400

    adds a scope for all the projects that don't belong to the current user.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
     belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
     has_many :project_features,  dependent: :destroy 
     has_many :users, -> {distinct},  through: :project_features
+    scope :where_not_mine, ->(user) {where.not("owner_id = ?", user)}
     accepts_nested_attributes_for :project_features, reject_if: proc { |attributes| attributes['name'].blank?  }
     validates_presence_of :title, :goal
 
--- app/models/project_feature.rb
+++ app/models/project_feature.rb
@@ -2,4 +2,9 @@ class ProjectFeature < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   validates :name, presence: true 
+  
+  def is_mine(user)
+    self.user_id == user ?  true : false
+  end
+
 end

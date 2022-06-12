88f56568e1327ecbe157e944efab70d98277069b
Rahman Elsirafy <rahman2408@gmail.com>
2022-06-12 05:29:48 -0400

    changes routes for oAuth

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,7 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#login", as: "login"
   post "/login", to: "sessions#create"
   match "/logout", to: "sessions#logout", via: [:get, :post]
-  # match '/auth/:provider/callback', to: 'sessions#git_goin', via: [:get, :post]
-  get '/auth/:provider/callback', to: 'session#git_goin'
-  post '/auth/:provider/callback', to: 'session#git_goin'
+  match '/auth/:provider/callback', to: 'sessions#git_goin', via: [:get, :post]
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
